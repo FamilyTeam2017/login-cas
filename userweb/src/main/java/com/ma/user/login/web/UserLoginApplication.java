5178cf502151085b469caa945e23bf470143fc59
maxiaogang <maxiaogang@youzan.com>
2018-01-07 14:50:08 +0800

    构建启动框架

---
+++ userweb/src/main/java/com/ma/user/login/web/UserLoginApplication.java
@@ -0,0 +1,12 @@
+package com.ma.user.login.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.ma")
+public class UserLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserLoginApplication.class);
+    }
+}
